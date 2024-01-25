2890de94289c29a9d6d8b4ad30af6593508ca525
0xkyng <teryima127@gmail.com>
2024-01-25 12:35:06 +0100

    chore:replace contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,17 @@
+require('dotenv').config();
 require("@nomicfoundation/hardhat-toolbox");
 
-/** @type import('hardhat/config').HardhatUserConfig */
+// Go to https://infura.io, sign up, create a new API key
+// in its dashboard, and replace "KEY" with it
+const INFURA_API_KEY = "INFURA_API_KEY";
+const SEPOLIA_PRIVATE_KEY = "SEPOLIA_PRIVATE_KEY";
+
 module.exports = {
-  solidity: "0.8.19",
+  solidity: "0.8.23",
+  networks: {
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [`0x${PRIVATE_KEY}`]
+    }
+  }
 };
